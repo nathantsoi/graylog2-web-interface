27820dc86c45776a7c065c4151a25f82676b6ecd
nathan <nathan@vertile.com>
2013-08-23 12:21:52 -0700

    create a secret token on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,3 +19,28 @@ set :branch, fetch(:branch, 'master')
 before 'deploy:restart', 'graylog:yaml:install'
 
 after :deploy, 'deploy:cleanup'
+before 'deploy:restart', 'deploy:symlink_secret_token'
+
+namespace :deploy do
+  task :create_secret_token do
+    secret_token_contents = "Graylog2WebInterface::Application.config.secret_token = \"#{::SecureRandom.hex(128)}\""
+    file = "#{shared_path}/secret_token.rb"
+    unless remote_file_exists?(file)
+      run "echo -e '#{secret_token_contents}' > #{file}"
+    end
+  end
+  task :symlink_secret_token do
+    create_secret_token
+    run "ln -snf #{shared_path}/secret_token.rb #{current_path}/config/initializers/"
+  end
+end
+
+# http://stackoverflow.com/questions/1661586/how-can-you-check-to-see-if-a-file-exists-on-the-remote-server-in-capistrano
+# .. with fixes
+def remote_file_exists?(path)
+  results = []
+  invoke_command("if [ -e '#{path}' ]; then echo -n 'true'; fi") do |ch, stream, out|
+    results << (out == 'true')
+  end
+  results.length > 0 && results.all?
+end
